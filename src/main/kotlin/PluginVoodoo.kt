4629be601c3dec9b211efa60ff7b9515a2b753d7
AlerHugu3s <axiasiming@gmail.com>
2021-09-30 17:26:52 +0800

    v1.5.0版本更新 增加随机禁言 自我禁言 以及 全员解除禁言 功能 增加周易占卜功能

--- src/main/kotlin/PluginVoodoo.kt
+++ src/main/kotlin/PluginVoodoo.kt
@@ -11,7 +11,7 @@ object PluginVoodoo : KotlinPlugin(
     JvmPluginDescription(
         id = "org.AlerHughes.plugin-voodoo",
         name = "PluginVoodoo",
-        version = "1.2.0",
+        version = "1.5.0",
     ) {
         author("AlerHughes")
     }
